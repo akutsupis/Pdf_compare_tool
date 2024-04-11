90054480467411dd2620acc5fcd52093d355a815
Alex Kutsupis <axak55@outlook.com>
2024-04-11 01:48:34 -0400

    Update to functioning v1

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-from requests import request
+from requests_func import request
 from pdfhandler import load_pdf
 from remove_stopwords import remove_stopwords
 from summarizer_func import summarize_text
 
 
+def main(request_for_proposals, proposal, system):
+    # Load PDF for city plan and proposal
+    plan_page_count, request_text = load_pdf(request_for_proposals)
+    proposal_page_count, proposal_text = load_pdf(proposal)
+
+    # if mode_switch == 'stopwords':
+    #     # test option, removal of stopwords to reduce tokens
+    #     request_text = remove_stopwords(request_text)
+    #     proposal_text = remove_stopwords(proposal_text)
+    #     return request_text, proposal_text
+    #
+    # if mode_switch == 'summarize':
+    #     # Summarize the master plan and proposal
+    #     request_text = summarize_text(request_text)
+    #     proposal_text = summarize_text(proposal_text)
+    #     return request_text, proposal_text
+
+    # Make a request to the OpenAI API
+    response = request('gpt-3.5-turbo',
+                       system,
+                       request_text,
+                       proposal_text)
+
+    return print(response)
+
+
 if __name__ == "__main__":
-    def main(request_for_proposals, proposal, mode_switch=None):
-        # Load PDF for city plan and proposal
-        plan_page_count, request_text = load_pdf(request_for_proposals)
-        proposal_page_count, proposal_text = load_pdf(proposal)
-
-        if mode_switch == 'stopwords':
-            # test option, removal of stopwords to reduce tokens
-            plan_text = remove_stopwords(request_text)
-            proposal_text = remove_stopwords(proposal_text)
-            return plan_text, proposal_text
-
-        if mode_switch == 'summarize':
-            # Summarize the master plan and proposal
-            plan_text = summarize_text(request_text)
-            proposal_text = summarize_text(proposal_text)
-            return plan_text, proposal_text
-
-        print(proposal_text)
-        # # Make a request to the OpenAI API
-        # response = request('gpt-3.5-turbo',
-        #                    'Compare the proposal to the master plan.',
-        #                    request_for_proposals,
-        #                    proposal_text)
-
-
-main('PDFs/State_Street_Campus_Garage_RFP.pdf',
-     'PDFs/Greystar_response.pdf')
-
+    main('PDFs/State_Street_Campus_Garage_RFP.pdf',
+         'PDFs/Greystar_response.pdf',
+         'You are an urban planner. Your goal is to compare the proposal to the request for proposal. Does the proposal fully align with the request? Compare and summarize. What suggestions would you make to improve the proposal, given the request and the local context you are aware of for this area?')
--- requests_func.py
+++ requests_func.py
@@ -1,3 +1,8 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+
 def request(model, system, rfp, proposal):
     """
     This function makes a request to the OpenAI API and returns the response.
@@ -5,15 +10,23 @@ def request(model, system, rfp, proposal):
     Parameters:
     model (str): The model to be used for the request. For example, 'gpt-3.5-turbo'.
     system (str): The system message to initialize the conversation.
-    master_plan (str): The city's master plan.
+    rfp (str): The city's request for proposal.
     proposal (str): The land developer's proposal.
 
     Returns:
     str: The message returned by the model. If an error occurs during the request, the function returns None.
 
     """
+    load_dotenv()  # take environment variables from .env.
+
+    # Code of your application, which uses environment variables (e.g. from `os.environ` or
+    # `os.getenv`) as if they came from the actual environment.
+
+    API_KEY = os.getenv('OPENAI_API_KEY')
+    if not API_KEY:
+        print("Error: The OPENAI_API_KEY environment variable is not set.")
+        return None
     try:
-        from openai import OpenAI
         # Initialize the OpenAI client with the base URL and API key
         client = OpenAI(base_url="https://openai.vocareum.com/v1", api_key=API_KEY)
 
@@ -29,10 +42,9 @@ def request(model, system, rfp, proposal):
          )
 
         # Return the message from the first choice in the completion
-        return completion.choices[0].message
+        return (completion)
+        #completion.choices[0].message)
 
     except Exception as e:
 
-        print(f"An error occurred: {e}")
-
-        return None
+        return f"An error occurred: {e}"
